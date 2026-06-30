2fed0fb2cb28c46f67aec6f5b322e712f7d68fb3
Rufayl <rufayl31.2002@gmail.com>
2026-06-30 11:21:20 -0300

    fix: add setup.py for bench install, app_email/license in hooks

--- garib_ui/hooks.py
+++ garib_ui/hooks.py
@@ -3,6 +3,8 @@ app_title = "Garib UI"
 app_publisher = "Garib Appliances"
 app_description = "Inventory back-office UI for Garib Appliances"
 app_version = "0.0.1"
+app_email = "rufayl31.2002@gmail.com"
+app_license = "MIT"
 
 # Makes /garib serve garib_ui/www/garib.html
 website_route_rules = [
---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='garib_ui',
+    version='0.0.1',
+    packages=find_packages(),
+)
